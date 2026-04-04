00eb99de162f056b2e5f3c009463c87f6b56ff2a
Sally Cochenour <sally@doghouse-games.com>
2026-04-04 17:28:57 -0400

    Change default character wins
    I decided I prefer excluding them over nerfing them, because the character wins condition is easier for world generation to fulfill but is much less fun to do than the other condition

--- apworld/options.py
+++ apworld/options.py
@@ -36,8 +36,8 @@ class CharWinsCount(NamedRange):
     """
     Alter the number of wins required for any "100 wins playing as X" challenges.
 
-    - **Default**: Require only 5 wins per character.
     - **Exclude**: Require 100 wins per character, but exclude from logic. Every challenge that has this condition uses it as an alternative, so this doesn't remove any locations.
+    - **Nerfed**: Require only 5 wins per character, in logic.
     - **Vanilla**: Require 100 wins per character, in logic. Not recommended.
     """
 
@@ -45,11 +45,11 @@ class CharWinsCount(NamedRange):
     display_name = "Character Wins Count"
     range_start = 1
     range_end = 100
-    default = 5
+    default = 0
     special_range_names = {
-        "default": 5,
-        "vanilla": 100,
         "exclude": 0,
+        "nerfed": 5,
+        "vanilla": 100,
     }
 
 
