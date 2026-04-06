fd73ffd2ad5566621c73cf31d5bc9ab6cd468e26
Sally Cochenour <sally@doghouse-games.com>
2026-04-06 18:58:26 -0400

    Add setup guide

--- apworld/web_world.py
+++ apworld/web_world.py
@@ -5,9 +5,19 @@ from .options import option_groups
 
 
 class RingRacersWebWorld(WebWorld):
-    game = "Dr. Robotnik's Ring Racers"
-    theme = "grassFlowers"
-    option_groups = option_groups
-    #option_presets = option_presets
-    location_descriptions = location_descriptions
-    item_descriptions = item_descriptions
+	game = "Dr. Robotnik's Ring Racers"
+	theme = "grassFlowers"
+	option_groups = option_groups
+	#option_presets = option_presets
+	location_descriptions = location_descriptions
+	item_descriptions = item_descriptions
+
+	setup_en = Tutorial(
+		"Multiworld Setup Guide",
+		"A guide to setting up Dr. Robotnik's Ring Racers for MultiWorld.",
+		"English",
+		"setup_en.md",
+		"setup/en",
+		["TehRealSalt"],
+	)
+
